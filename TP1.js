11f97b5088df1fd58f63478eaa03c3914d6e5a5d
Herlyck <mrcyrax64@gmail.com>
2026-07-05 19:45:41 -0300

    pruebas

--- TP1.js
+++ TP1.js
@@ -2,59 +2,69 @@ const leer = require("prompt-sync")();
 
 const MAX_FILA = 10;
 const MAX_COL = 10;
-const SIMB_CASILLA_VACIA = " ." ;
+const SIMB_CASILLA_VACIA = " .";
 const SIMB_JUGADOR = " J";
 const SIMB_ENEMIGO = " X";
 const SIMB_COMPUERTA = " C";
 const SIMB_SALIDA = " S";
 const tablero = [];
 
-const enemigo ={
+const enemigo = {
     posY: 9,
-    posX:9
-}
+    posX: 9
+};
 
 const jugador = {
     posY: 5,
     posX: 0
-    
-}
+
+};
 
 const compuertaSeguridad = {
     posY: 5, //fila
     posX: 9  //col
-    
-}
+
+};
 
 const salida = {
     posY: 0,
     posX: 0
-    
-}
+
+};
 
 
 function main() {
     crearTablero();
     console.clear();
-    tablero[jugador.posY][jugador.posX] = SIMB_JUGADOR;
-    tablero[compuertaSeguridad.posY][compuertaSeguridad.posX] = SIMB_COMPUERTA;
-    tablero[salida.posY][salida.posX] = SIMB_SALIDA;
-    tablero[enemigo.posY][enemigo.posX] = SIMB_ENEMIGO;
+
+
+    for (let intentos = 0; intentos < 50; intentos++) {
+        tablero[jugador.posY][jugador.posX] = SIMB_JUGADOR;
+        tablero[compuertaSeguridad.posY][compuertaSeguridad.posX] = SIMB_COMPUERTA;
+        tablero[salida.posY][salida.posX] = SIMB_SALIDA;
+        tablero[enemigo.posY][enemigo.posX] = SIMB_ENEMIGO;
+        
+        // if (jugador.posY === enemigo.posY && jugador.posX === enemigo.posX) {
+        //     intentos = 50;
+        // }
+
+    }
+
+
+
     mostrarTablero();
     leer();
-    console.clear();
-    tablero[--enemigo.posY][--enemigo.posX]=SIMB_ENEMIGO;
-    mostrarTablero();
-    
 
-    
-    
-    for(let intentos = 0;intentos<50;intentos++){
-        if (jugador.posY === enemigo.posY && jugador.posX ===enemigo.posX){
-            intentos=50;
-        } 
-        
-    }
+
+
+    console.clear();
+    // tablero[--enemigo.posY][--enemigo.posX] = SIMB_ENEMIGO; tengo q actualizar estoy en prueba
+    mostrarTablero();
+
+
+
+
+
 
 
 
